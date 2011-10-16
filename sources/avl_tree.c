e18bd3a269dff217e2bcdb9a7d528c5f3ed4923f
Noemj <olli.rissanen@helsinki.fi>
2011-10-16 13:00:19 +0300

    added test folder

--- sources/avl_tree.c
+++ sources/avl_tree.c
@@ -10,8 +10,9 @@ struct avl_tree_node
 	struct avl_tree_node* right;
 };
 
-struct avl_tree_node* avl_tree_create(char* key, int value);
-void avl_tree_destroy(struct avl_tree_node* root);
-void avl_tree_insert(struct avl_tree_node* root, char* key, int value);
-int* avl_tree_find(struct avl_tree_node* root, char* key);
+struct avl_tree_node* avl_tree_create(char* key, int value){}
+void avl_tree_destroy(struct avl_tree_node* root){}
+void avl_tree_insert(struct avl_tree_node* root, char* key, int value){}
+int* avl_tree_find(struct avl_tree_node* root, char* key){}
+
 
