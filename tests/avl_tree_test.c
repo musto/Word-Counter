2987949ee569344d47411b8556eddd49b3656e33
DaeD <eat@spam.com>
2011-10-16 14:15:49 +0300

    Added new test.

--- tests/avl_tree_test.c
+++ tests/avl_tree_test.c
@@ -25,10 +25,39 @@ void test_finding()
 	avl_tree_destroy(root);
 }
 
+void test_inserting_and_finding()
+{
+	struct avl_tree_node* root;
+	int* value;
+
+	root = avl_tree_create("esa", 3);
+	avl_tree_insert(root, "vesa", 4);
+	avl_tree_insert(root, "vvv", 5);
+	avl_tree_insert(root, "aaa", 7);
+
+	value = avl_tree_find(root, "vesa");
+	assertIntEquals(4, *value);
+
+	value = avl_tree_find(root, "esa");
+	assertIntEquals(3, *value);
+
+	value = avl_tree_find(root, "vvv");
+	assertIntEquals(5, *value);
+
+	value = avl_tree_find(root, "aaa");
+	assertIntEquals(7, *value);
+
+	value = avl_tree_find(root, "bbb");
+	assertNull(value);
+
+	avl_tree_destroy(root);
+}
+
 int main(void)
 {
 	test_creation();
 	test_finding();
+	test_inserting_and_finding();
 
 	return 0;
 }
