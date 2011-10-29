27591f3bd5194bd8302aa957a2090bc0e7d34178
Noemj <olli.rissanen@helsinki.fi>
2011-10-29 23:08:27 +0300

    Added an early implementation of int_counts

--- sources/int_counts.c
+++ sources/int_counts.c
@@ -4,20 +4,29 @@
 
 struct int_counts
 {
+int* parray;
+int key;
+int value;
 };
 
 struct int_counts* int_counts_create(void)
 {
-	return NULL;
+int* array = malloc(sizeof(int)*100);
+struct int_counts* container = malloc(sizeof(struct int_counts));
+container->key = 0;
+container->parray = array;
+container->value = 0;
+return container;
 }
 
 void int_counts_add(struct int_counts* container, int key)
 {
+container->parray[key]++;
 }
 
 int int_counts_get(struct int_counts* container, int key)
 {
-	return 0;
+return container->parray[key];
 }
 
 int int_counts_min(struct int_counts* container)
@@ -26,11 +35,11 @@ int int_counts_min(struct int_counts* container)
 }
 
 int int_counts_max(struct int_counts* container)
-{
+{	
 	return -1;
 }
 
 void int_counts_destroy(struct int_counts* container)
 {
+free(container->parray);
 }
-
