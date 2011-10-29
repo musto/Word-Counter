b3562cef6fa9ff0e08cbc7d500db0b4121ad31b8
Noemj <olli.rissanen@helsinki.fi>
2011-10-29 23:41:50 +0300

    Updated int_counts.c

--- sources/int_counts.c
+++ sources/int_counts.c
@@ -6,34 +6,34 @@
 
 struct int_counts
 {
-int* parray;
-int value;
-int min;
-int max;
+	int* parray;
+	int value;
+	int min;
+	int max;
 };
 
 struct int_counts* int_counts_create(void)
 {
-int* array = malloc(sizeof(int)*100);
-struct int_counts* container = malloc(sizeof(struct int_counts));
-container->parray = array;
-container->value = 0;
-return container;
+	int* array = malloc(sizeof(int)*100);
+	struct int_counts* container = malloc(sizeof(struct int_counts));
+	container->parray = array;
+	container->value = 0;
+	return container;
 }
 
 void int_counts_add(struct int_counts* container, int key)
 {
-container->value++;
-container->parray[key]++;
-if (container->value > container->max)
-	container->max = container->value;
-if (container->value < container->min)
-	container->min = container->value;
+	container->value++;
+	container->parray[key]++;
+	if (container->value > container->max)
+		container->max = container->value;
+	if (container->value < container->min)
+		container->min = container->value;
 }
 
 int int_counts_get(struct int_counts* container, int key)
 {
-return container->parray[key];
+	return container->parray[key];
 }
 
 int int_counts_min(struct int_counts* container)
@@ -48,5 +48,5 @@ int int_counts_max(struct int_counts* container)
 
 void int_counts_destroy(struct int_counts* container)
 {
-free(container->parray);
+	free(container->parray);
 }
