1bc29283fd2f967689b7e2fb0e161ac473ebb5ca
Noemj <olli.rissanen@helsinki.fi>
2011-10-29 23:36:57 +0300

    Added comments for int_counts.h

--- sources/int_counts.c
+++ sources/int_counts.c
@@ -1,19 +1,21 @@
 #include "int_counts.h"
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct int_counts
 {
 int* parray;
-int key;
 int value;
+int min;
+int max;
 };
 
 struct int_counts* int_counts_create(void)
 {
 int* array = malloc(sizeof(int)*100);
 struct int_counts* container = malloc(sizeof(struct int_counts));
-container->key = 0;
 container->parray = array;
 container->value = 0;
 return container;
@@ -21,7 +23,12 @@ return container;
 
 void int_counts_add(struct int_counts* container, int key)
 {
+container->value++;
 container->parray[key]++;
+if (container->value > container->max)
+	container->max = container->value;
+if (container->value < container->min)
+	container->min = container->value;
 }
 
 int int_counts_get(struct int_counts* container, int key)
@@ -31,12 +38,12 @@ return container->parray[key];
 
 int int_counts_min(struct int_counts* container)
 {
-	return -1;
+	return container->min;
 }
 
 int int_counts_max(struct int_counts* container)
 {	
-	return -1;
+	return container->max;
 }
 
 void int_counts_destroy(struct int_counts* container)
