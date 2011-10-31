bd43d4978a1a4404ac6722ebdcd8c66efe874dd3
Noemj <olli.rissanen@helsinki.fi>
2011-10-31 22:06:31 +0200

    Added documentations

--- sources/parser.h
+++ sources/parser.h
@@ -8,7 +8,7 @@
  *
  * For each word it calls the given function pointer (process_word). Parameter for this function call is the word.
  * 
- * For each sentence it calls the given function pointer (process_sentence). Parameter for this function call is the sentence length.
+ * For each sentence it calls the given function pointer (process_sentence). Parameter for this function call is the sentences length.
  */
 
 void parse_words_and_sentence_lengths(FILE* input, void (*process_word)(const char* word), void (*process_sentence)(int length));
